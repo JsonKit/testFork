002778d35d81712d7f70f7cf2c2a38d77462e8be
fantiancai <ftc4039@163.com>
2022-11-17 15:49:44 +0800

    init: 初始化项目

---
+++ TestFork/TestFork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestFork
+//
+//  Created by Json on 2022/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
